c93231fa757e0648b422c180f0bb79d5d098d804
Mariano De Simone <desimone.mariano@gmail.com>
2023-03-08 00:37:56 +0100

    Vercel Plugin Conf

--- server/config/plugins.js
+++ server/config/plugins.js
@@ -2,10 +2,9 @@ module.exports = ({ env }) => ({
   "vercel-deploy": {
     enabled: true,
     config: {
-      deployHook:
-        "https://api.vercel.com/v1/integrations/deploy/prj_bGWnDhsECZtVwKLLgi96P29qbbKu/Br7UK7YMQw",
-      apiToken: "dGDbPALg7bT5jJtr7ntu3nLM",
-      appFilter: "joyas-valencia-server",
+      deployHook: process.env.VERCEL_DEPLOY_PLUGIN_HOOK,
+      apiToken: process.env.VERCEL_DEPLOY_PLUGIN_API_TOKEN,
+      appFilter: process.env.VERCEL_DEPLOY_PLUGIN_APP_FILTER,
       teamFilter: "",
       roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
     },
