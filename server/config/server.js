e6ad871769dd1aac687b996d4433736879d22bb4
Mariano De Simone <desimone.mariano@gmail.com>
2023-03-08 12:22:53 +0100

    Do Server

--- server/config/server.js
+++ server/config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env("HOST", "https://strapi-app-2qbph.ondigitalocean.app"),
+  host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   app: {
     keys: env.array("APP_KEYS"),
