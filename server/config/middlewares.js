cf92c4da448d9bc76aa2248ee062784c78e8478b
Mariano De Simone <desimone.mariano@gmail.com>
2023-03-08 19:45:24 +0100

    Cloudinary

--- server/config/middlewares.js
+++ server/config/middlewares.js
@@ -1,6 +1,19 @@
 module.exports = [
   "strapi::errors",
-  "strapi::security",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        userDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https"],
+          "img-src": ["'self'", "data:", "blob:", "https://res.cloudinary.com"],
+          "media-src": ["'self'", "data:", "blob:"],
+          upgradeInsecure: null,
+        },
+      },
+    },
+  },
   "strapi::cors",
   "strapi::poweredBy",
   "strapi::logger",
--- server/config/plugins.js
+++ server/config/plugins.js
@@ -9,4 +9,18 @@ module.exports = ({ env }) => ({
       roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
     },
   },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
 });
