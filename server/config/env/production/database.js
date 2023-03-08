c69098098d21598f0736f90b34c51bd60b8e9d50
Mariano De Simone <desimone.mariano@gmail.com>
2023-03-08 10:48:08 +0100

    Merge Repos

--- server/config/env/production/database.js
+++ server/config/env/production/database.js
@@ -9,11 +9,7 @@ module.exports = ({ env }) => ({
       password: env("DATABASE_PASSWORD", "strapi"),
       schema: env("DATABASE_SCHEMA", "public"), // Not required
       ssl: {
-<<<<<<< HEAD
         ca: env("DATABASE_CA"),
-=======
-        ca: env('DATABASE_CA'),
->>>>>>> 2f1b48c6dc38570c27f42d3606865dc6ea294d2f
       },
     },
     debug: false,
