5a4f584b766a1bff0cf3018513f03c0923006407
Mariano De Simone <desimone.mariano@gmail.com>
2023-03-08 11:45:52 +0100

    Do Server  URL

--- server/config/server.js
+++ server/config/server.js
@@ -1,10 +1,11 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  url: "https://hammerhead-app-wxq3g.ondigitalocean.app",
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
 });
