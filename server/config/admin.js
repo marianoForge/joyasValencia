d4c6920f09fe9a8c16d885906e96feb9434325fd
Mariano De Simone <desimone.mariano@gmail.com>
2023-03-08 02:52:08 +0100

    Do Server 2

--- server/config/admin.js
+++ server/config/admin.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env("ADMIN_JWT_SECRET", process.env.ADMIN_JWT_SECRET),
+    secret: env("ADMIN_JWT_SECRET", "i2s+jiSG7GhBqA0seNqKNg=="),
   },
   apiToken: {
-    salt: env("API_TOKEN_SALT", process.env.API_TOKEN_SALT),
+    salt: env("API_TOKEN_SALT", "ZeqDzdClTXJwqom2j5SI3Q=="),
   },
   transfer: {
     token: {
-      salt: env("TRANSFER_TOKEN_SALT", process.env.TRANSFER_TOKEN_SALT),
+      salt: env("TRANSFER_TOKEN_SALT", "XdL3r3dkdO9p160RFkZXKg=="),
     },
   },
 });
