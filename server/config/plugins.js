e37fa71ef584be1b549a3f8f0f28a9f54db47ae1
Mariano De Simone <desimone.mariano@gmail.com>
2023-03-09 23:50:11 +0100

    Cloudinary 3

--- server/config/plugins.js
+++ server/config/plugins.js
@@ -17,6 +17,10 @@ module.exports = ({ env }) => ({
         api_key: env("CLOUDINARY_KEY"),
         api_secret: env("CLOUDINARY_SECRET"),
       },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
     },
   },
 });
